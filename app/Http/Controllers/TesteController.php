c495a8dd8e492a2150bd1f2284ed6816c09bb9d1
2Ten João Victor <2tenjoaovictor@esa>
2020-05-21 09:50:04 -0300

    Teste

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -2,17 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Utilitarios\SmbClientPhp;
-use App\Models\Alunos;
-use App\Models\AlunosCurso;
-use App\Models\AlunosSitDiv;
 use App\Models\Areas;
-use App\Models\ImagemAluno;
+use App\Models\Fatd;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Response;
-use PhpOffice\PhpSpreadsheet\IOFactory;
-use Route;
-use View;
 
 class TesteController extends Controller
 {
@@ -23,12 +15,7 @@ class TesteController extends Controller
      */
     public function index()
     {
-        $headers = array(
-            'Content-Type: application/pdf',
-        );
-        //Response::download(storage_path().'/CitrixHypervisor-8.1.0-install-cd.iso', 'CitrixHypervisor-8.1.0-install-cd.iso', ['content-type' => 'application/iso']);
-        Response::download(storage_path().'/CitrixHypervisor-8.1.0-install-cd.iso', 'teste.iso', $headers);
-        
+        dd(Areas::all());
     }
 
     /**
