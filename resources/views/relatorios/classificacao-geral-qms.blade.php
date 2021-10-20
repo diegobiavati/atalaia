96097a384d9ad2363a102babe059555f597afab5
3xpedition <jvgs_o.o@live.com>
2021-10-20 16:11:19 -0300

    Correção do problema do TFM no Relatório de Classificação Geral

--- resources/views/relatorios/classificacao-geral-qms.blade.php
+++ resources/views/relatorios/classificacao-geral-qms.blade.php
@@ -11,7 +11,6 @@
 <div style="width: 1600px; margin: 4px auto; page-break-after: always; text-align: center;">
    
         @if($_GET['options_class_geral']==10)
-
             @foreach($qmss as $qms)
                 <h4 style="text-align: center; margin-top: 18px;">CLASSIFICAÇÃO POR QMS</h4>
                     <h4 style="text-align: center;">{{$qms->qms}}</h4>
@@ -21,9 +20,10 @@
                             <td style="border: 1px solid #000; padding: 6px; text-align: center;"><b>NOME COMPLETO</b></td>
                             <td style="border: 1px solid #000; padding: 6px; text-align: center;"><b>SEGMENTO</b></td>
                             <td style="border: 1px solid #000; padding: 6px; text-align: center;"><b>UETE</b></td>
-                            @foreach ($disciplinas as $disciplina)
-                            
-                                <td style="border: 1px solid #000; padding: 6px; text-align: center;"><b>ND<br />{{$disciplina->nome_disciplina_abrev}}</b></td>                            
+                            @foreach ($disciplinas as $key => $disciplina)
+                                @if(is_numeric($key) && $disciplina['tfm'] == 'N')
+                                    <td style="border: 1px solid #000; padding: 6px; text-align: center;"><b>ND<br />{{$disciplina->nome_disciplina_abrev}}</b></td>                            
+                                @endif
                             @endforeach
                             <td style="border: 1px solid #000; padding: 6px; text-align: center;"><b>ND<br />TFM</b></td>
                             <td style="border: 1px solid #000; padding: 6px; text-align: center;"><b>N1</b></td>
@@ -35,7 +35,8 @@
                         <?php
                             $i=1;
                         ?>
-                        @foreach($listaAlunoQms[$qms->qms_sigla] as $aluno)
+                        @foreach($listaAlunoQms[$qms->qms_sigla] as $aluno) 
+                        
                                 @unset($media_tfm)
                                 <tr> 
                                     <td style="border: 1px solid #000; padding: 6px; text-align: center;">{{ $aluno->numero }}</td>
@@ -43,32 +44,28 @@
                                     <td style="border: 1px solid #000; padding: 6px; text-align: center;">{{ $aluno->sexo }}</td>
                                     <td style="border: 1px solid #000; padding: 6px; text-align: center;">{{ $aluno->omct->sigla_omct }}</td>
 
-                                    @foreach ($disciplinas as $disciplina)
-                                        <td style="border: 1px solid #000; padding: 6px; text-align: center;">                                
-                                        @foreach($aluno->data_demonstrativo as $key => $data_demonstrativo)
-                                            @if(is_array($data_demonstrativo)) 
-                                                @if($disciplina->id == $data_demonstrativo['disciplina_id'])
-
+                                    @foreach ($disciplinas as $key => $disciplina)
+                                        @if(is_numeric($key) && $disciplina['tfm'] == 'N')
+                                            <td style="border: 1px solid #000; padding: 6px; text-align: center;">
+                                            @foreach($aluno->data_demonstrativo as $data_demonstrativo)
+                                                @if(isset($data_demonstrativo['disciplina_id']) && $disciplina->id == $data_demonstrativo['disciplina_id'])
                                                     @if(isset($data_demonstrativo['media_sem_peso']))
                                                         {{ number_format($data_demonstrativo['media_sem_peso'], 3, ',', '') }}
                                                     @else
                                                         {{ (is_numeric($data_demonstrativo['media']) ? number_format($data_demonstrativo['media'], '3', ',', '') : $data_demonstrativo['media']) }}
                                                     @endif
-                                                    
-                                                    @if(isset($media_tfm))
-                                                        @break
-                                                    @endif
+                                                    @break
                                                 @endif
+                                            @endforeach
 
-                                                @if($data_demonstrativo['disciplina_id'] == 99999)
-                                                    @php    
-                                                        $media_tfm = number_format($data_demonstrativo['media'], '3', ',', '');
-                                                    @endphp
-                                                @endif
-                                            @endif
-                                        @endforeach
-                                        </td>
+                                            </td>                            
+                                        @endif
+                                        
                                     @endforeach
+
+                                    @php    
+                                        $media_tfm = number_format($aluno->data_demonstrativo['avaliacoes_tfm']['media_tfm'], '3', ',', '');
+                                    @endphp
                                     
                                     <td style="border: 1px solid #000; padding: 6px; text-align: center;">{{ $media_tfm or '--'}}</td>
                                     <td style="border: 1px solid #000; padding: 6px; text-align: center;">{{ number_format($aluno->classificacao->nota_final_arredondada, '3', ',', '') }}</td>
