6cd4ab553b2430f714a76e0656ebaf4160be3fa9
3xpedition <jvgs_o.o@live.com>
2024-02-06 16:07:59 -0300

    Gavião-Correção Erro

--- resources/views/ajax/chkbox-ano-qms.blade.php
+++ resources/views/ajax/chkbox-ano-qms.blade.php
@@ -1,6 +1,8 @@
+@isset($qmss)
 @foreach($qmss as $qms)
-    <div class="custom-control custom-checkbox" style="display: inline-block; margin: 0 10px 12px 0;">
-        <input type="checkbox" class="custom-control-input omcts" id="qms_{{$qms->id}}" name="qmss[]" value="{{$qms->id}}" />
-        <label class="custom-control-label" for="qms_{{$qms->id}}">{{$qms->qms}}</label>
-    </div>
+<div class="custom-control custom-checkbox" style="display: inline-block; margin: 0 10px 12px 0;">
+    <input type="checkbox" class="custom-control-input omcts" id="qms_{{$qms->id}}" name="qmss[]" value="{{$qms->id}}" />
+    <label class="custom-control-label" for="qms_{{$qms->id}}">{{$qms->qms}}</label>
+</div>
 @endforeach
+@endisset
