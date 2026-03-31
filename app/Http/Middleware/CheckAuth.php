46d6d9fd97fad3885dec4170566d9e179c174f6f
3xpedition <jvgs_o.o@live.com>
2026-03-31 13:39:14 -0300

    Gavião-Correção de Middleware

--- app/Http/Middleware/CheckAuth.php
+++ app/Http/Middleware/CheckAuth.php
@@ -3,33 +3,35 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\URL;
 
 class CheckAuth
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if(session()->get('url_atual')) {
-            session()->put('url_anterior', session()->get('url_atual'));
-        } 
-        session()->put('url_atual', URL::current());
+        if (session()->has('url_atual')) {
+            session()->put('url_anterior', session('url_atual'));
+        }
+
+        session()->put('url_atual', $request->fullUrl());
 
         if (!auth()->check()) {
-            
-            if(preg_match('{gaviao}', $request->path())){
+
+            $host = $request->getHost();
+
+            if (strpos($host, 'gaviao') !== false) {
                 return redirect('gaviao');
-            }else{
-                return redirect('login');
             }
-            
+
+            return redirect('login');
         }
+
         return $next($request);
     }
 }
